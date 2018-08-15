1bf729d750e35202e7ffa1fd8482308495984326
Eduardo Galego <egalego@ext.leroymerlin.com.br>
2018-08-15 08:33:28 -0300

    First commit

---
+++ api-example/src/test/java/fiap/lab/microservices/apiexample/ApiExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package fiap.lab.microservices.apiexample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApiExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
